343cec921d3995037ee30f082dd526daf7854638
Javier Alejandro Ruiz <jaaruizgu@gmail.com>
2023-07-12 14:22:03 -0500

    Change amount of vms and ports

--- netbox_proxbox/proxbox_api_v2/port_scanner.py
+++ netbox_proxbox/proxbox_api_v2/port_scanner.py
@@ -141,7 +141,7 @@ class VMPortScanner:
     async def process_ip(vm, ip):
         host = str(ip.address.ip)
         ports_range = range(1, 65535)
-        ports_open = await VMPortScanner.process_ports(host, ports_range, 10)
+        ports_open = await VMPortScanner.process_ports(host, ports_range, 50)
         services = []
         for i in ports_open:
             s = await asyncio.to_thread(VMPortScanner.set_service_to_vm, vm, ip, i)
@@ -181,7 +181,7 @@ class VMPortScanner:
                 ports_subset = ports[offset:offset1]
                 runner = []
                 for j in ports_subset:
-                    runner.append(VMPortScanner.test_port_number(host, j, 4))
+                    runner.append(VMPortScanner.test_port_number(host, j, 3))
                 result = await asyncio.gather(*runner, return_exceptions=True)
                 # result.append((True, host, 12345))
                 # result.append((True, host, 12346))
