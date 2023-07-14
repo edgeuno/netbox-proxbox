9ececfc515cae39d37ec889daf6df778ec5373d1
Javier Alejandro Ruiz <jaaruizgu@gmail.com>
2023-07-14 18:16:10 -0500

    Change to a multicore - multi thread model for executing the port scanner

--- netbox_proxbox/proxbox_api_v2/port_scanner.py
+++ netbox_proxbox/proxbox_api_v2/port_scanner.py
@@ -377,7 +377,7 @@ class VMPortScannerSync:
             ports_to_scan = ports_to_scan + r
 
         ports_to_scan = VMPortScannerSync.random_list(ports_to_scan)
-        limit = 10000
+        limit = 2000
         total = len(ports_to_scan)
         pages = math.ceil( total / limit)
 
