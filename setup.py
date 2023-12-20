18839c707ff0f76b161ed80880f4703202bc5fc5
javier <javier.ruiz@edgeuno.com>
2023-12-20 11:21:25 -0500

    Add logs for the start of ech process

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ dev_requires = [
 
 setup(
     name="netbox-proxbox",
-    version="0.0.10",
+    version="0.0.11",
     author="Javier Alejandro Ruiz",  # Original Autor Emerson Felipe
     author_email="javier.ruiz@edgeuno.com",  # Original Autor Email emerson.felipe@nmultifibra.com.br
     description="Integration between Proxmox and Netbox",
